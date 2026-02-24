b7760492a0e6601448b4c5a67d95240d890b323e
Anikesh994 <anikeshkumar994@gmail.com>
2026-02-24 20:31:42 +0530

    convex_clerk auth

---
+++ src/app/(auth)/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Auth_layout = ({children}) => {
+  return (
+    <div className="flex justify-center pt-48">
+      {children}
+    </div>
+  )
+}
+
+export default Auth_layout
