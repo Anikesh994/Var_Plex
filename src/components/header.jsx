5c430a22521b0b7976e7ebf23b7d3b08070b60c7
Anikesh994 <anikeshkumar994@gmail.com>
2026-03-21 16:34:22 +0530

    schema

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,18 +5,22 @@ import Image from 'next/image';
 import { usePathname } from "next/navigation";
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
 import { Button } from './ui/button';
+import { useStoreUser } from '@/hooks/use-store-user';
+import { BarLoader } from "react-spinners";
 
 const Header = () => {
 
     const path =usePathname();
+    const {isLoading, isAuthenticated} = useStoreUser()
     if(path.includes("/editor")){
         return null;
     }
 
 
   return (
-    <header className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 text-nowrap ">
-        <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-full px-8 py-0 flex items-center justify-between gap-8">
+    <>
+    <header className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 text-nowrap  ">
+        <div className="backdrop-blur-md bg-white/10 border border-white/20 rounded-full px-8 py-3 flex items-center justify-between gap-8">
         <Link href="/" className="mr-10 md:mr-20" >
         <Image
         src ="/logo.png"
@@ -89,10 +93,19 @@ const Header = () => {
         </div>
 
         
-
+        {
+          isLoading && (
+            <div className="fixed bottom-0 left-6 w-3xl z-40 flex justify-center " >
+              <BarLoader width={"95%"} color="#06b6d4" cssOverride={{ borderRadius:'9999px',
+              height: '5px',
+            }}  />
+            </div>
+          )
+        }
         
 
     </header>
+    </>
   )
 }
 
